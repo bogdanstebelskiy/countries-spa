6b8a101b8d2efbcf2776f19e0d09d3ad66e61cad
bogdanstebelskiy <bogdanstebelskiy@gmail.com>
2024-05-02 01:07:27 +0300

    Added Country Card List

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,58 @@
 import { Controls } from "./components/Controls";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
+import { List } from "./components/List";
+import { Card } from "./components/Card";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { ALL_COUNTRIES } from "./config";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  console.log(countries);
+  useEffect(() => {
+    axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data));
+  }, []);
+
+  type CountryType = {
+    capital: string;
+    flags: { png: string; svg: string };
+    name: string;
+    population: number;
+    region: string;
+    info: { title: string; description: string }[];
+  };
+
   return (
     <>
       <Header />
       <Main>
         <Controls />
+        <List>
+          {countries.map((c: CountryType) => {
+            const countryInfo = {
+              img: c.flags.png,
+              name: c.name,
+              info: [
+                {
+                  title: "Population",
+                  description: c.population.toLocaleString(),
+                },
+                {
+                  title: "Region",
+                  description: c.region,
+                },
+                {
+                  title: "Capital",
+                  description: c.capital,
+                },
+              ],
+            };
+
+            return <Card key={c.name} {...countryInfo} />;
+          })}
+        </List>
       </Main>
     </>
   );
--- src/components/CustomSelect.tsx
+++ src/components/CustomSelect.tsx
@@ -17,10 +17,31 @@ export const CustomSelect = styled(Select).attrs({
     option: (provided, state) => ({
       ...provided,
       cursor: "pointer",
-      color: state.isSelected ? "var(--colors-bg)" : "var(--colors-text)",
+      color: "var(--colors-text)",
       backgroundColor: state.isSelected
         ? "var(--colors-bg)"
         : "var(--colors-ui-base)",
     }),
   },
-})``;
+})`
+  width: 200px;
+  border-radius: var(--radii);
+  font-family: var(--family);
+  border: none;
+
+  & > * {
+    box-shadow: var(--shadow);
+  }
+
+  & input {
+    padding-left: 0.25rem;
+  }
+
+  & * {
+    color: var(--colors-text) !important;
+  }
+
+  & > div[id] {
+    background-color: var(--colors-ui-base);
+  }
+`;
